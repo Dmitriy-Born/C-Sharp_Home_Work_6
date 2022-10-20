164f2fe38b601afe3cff2e17a5676ee2ade455e8
Dmitriy Born <1111born1111@gmail.com>
2022-10-20 12:43:06 +0300

    Ex_1

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 4
+
+Console.WriteLine("Введите количество элементов массива (целочисленное число): ");
+int Number = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Введите {Number} элементов массива: ");
+double[] Data = Zapol(Number);
+Console.WriteLine($"Заданный массив: [{String.Join(" | ", Data)}]");
+
+int count = 0;
+for(int i = 0; i < Data.Length; i++){
+    if (Data[i] > 0){
+        count++;
+    }
+}    
+
+Console.WriteLine($"Пользователь ввел {count} чисел больше 0");
+
+double[] Zapol(int Num){
+double[] collection = new double[Num];
+for(int i = 0; i < collection.Length; i++) collection[i] = double.Parse(Console.ReadLine()!);
+return collection;
+}
